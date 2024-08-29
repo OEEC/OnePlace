af2c829fc710e1adb4888ab49c88012991423f02
AngelZapata582 <angelzapata582@gmail.com>
2024-08-29 11:01:54 -0600

    Correccion
    - corrección de envíos de parámetros en conteo de actividad

--- OnePlace/Shared/DTOs/ContadorActividadDTO.cs
+++ OnePlace/Shared/DTOs/ContadorActividadDTO.cs
@@ -1,18 +1,14 @@
 ﻿using OnePlace.Shared.Filtros;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnePlace.Shared.DTOs
 {
     public class ContadorActividadDTO : Parametros_Busqueda_Gen
     {
-        public string Estacion { get; set; }
-        public string Empleado { get; set; }
-        public string Persona { get; set; }
-        public string Accion { get; set; }
-        public DateTime Fecha_Registro { get; set; }
+        public string Estacion { get; set; } = string.Empty;
+        public string Empleado { get; set; } = string.Empty;
+        public string Persona { get; set; } = string.Empty;
+        public string Accion { get; set; } = string.Empty;
+        public DateTime Fecha_Registro { get; set; } = DateTime.Now;
     }
 }
